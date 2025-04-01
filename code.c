c7c8fd084cb5864d2646e1d45083f25c2404e26c
Linh Huynh <linh.huynh@mths.ca>
2025-04-01 09:41:26 -0400

    Create code.c

---
+++ code.c
@@ -0,0 +1,45 @@
+/* 
+ * Created by Linh Huynh
+ * Created on April 2025
+ * 
+*/
+
+# include <Servo.h>
+
+
+const int TRIG_PIN = 3;  
+const int ECHO_PIN = 2; 
+
+Servo servoNumber1;
+  
+float duration, distance;
+
+void setup() {
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    Serial.begin(9600);
+    servoNumber1.attach(10);
+    servoNumber1.write(0);
+}
+
+void loop() {
+    servoNumber1.write(0);
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration*.0343)/2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    delay(100);
+  
+    if (distance < 50) {
+      servoNumber1.write(0);
+      delay(1000);
+      servoNumber1.write(90);
+      delay(1000);
+	    }
+}
